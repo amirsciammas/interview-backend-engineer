fecd51bbeeed9f0cfd7335921d0288940a44c8da
Luca Saccone <lu.saccone@gmail.com>
2022-05-06 13:58:09 +0200

    Create config files

---
+++ swaggerDef.js
@@ -0,0 +1,12 @@
+/* istanbul ignore next */
+// This file is an example, it's not functionally used by the module.This
+
+module.exports = {
+  openapi: '3.0.0',
+  info: {
+    // API information (required)
+    title: 'Adidas Backend assignment', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'Adidas Backend assignment API documentation' // Description (optional)
+  }
+}
